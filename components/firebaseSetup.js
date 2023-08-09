5343a9b31781478a6978c4a4257ba8bf55a96659
xNewz <63030068@kmitl.ac.th>
2023-08-09 19:07:17 +0700

    fixes: rename html file

--- components/firebaseSetup.js
+++ components/firebaseSetup.js
@@ -3,14 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP-fAfaJ4z_lJKdqU9ETV-DQaWhe3par0",
-  authDomain: "students-c8e78.firebaseapp.com",
-  databaseURL:
-    "https://students-c8e78-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "students-c8e78",
-  storageBucket: "students-c8e78.appspot.com",
-  messagingSenderId: "633160919265",
-  appId: "1:633160919265:web:6971727db38249f75eafa0",
+  apiKey: "************************************",
+  authDomain: "************************************",
+  databaseURL: "************************************",
+  projectId: "************************************",
+  storageBucket: "************************************",
+  messagingSenderId: "************************************",
+  appId: "************************************",
 };
 
 const app = initializeApp(firebaseConfig);
